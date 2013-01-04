483acb7c6838a84ad0df4af6a406d383dd518df7
GnomeffinWay <baseboss889@yahoo.com>
2013-01-04 03:43:42 -0600

    Revert "Sending msg for new king, not new mayor"
    This reverts commit 65b6dd23f1aadff2be6de52767a495aa97c5336e.

--- src/com/palmergames/bukkit/towny/command/TownyAdminCommand.java
+++ src/com/palmergames/bukkit/towny/command/TownyAdminCommand.java
@@ -530,7 +530,7 @@ public class TownyAdminCommand implements CommandExecutor {
 							}
 						}
 						TownyUniverse.getDataSource().saveTown(capital);
-						String[] msg = TownySettings.getNewKingMsg(newKing.getName(),nation.getName());
+						String[] msg = TownySettings.getNewMayorMsg(newKing.getName());
 						TownyMessaging.sendTownMessage(capital, msg);
 						// TownyMessaging.sendMessage(player, msg);
 					} catch (TownyException e) {
