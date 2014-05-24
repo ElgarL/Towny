f229eb3d8d03e8395624bb94b387316dc7673004
RemlapVII <alexander.palmer92@gmail.com>
2014-05-24 12:45:03 -0400

    Flag War Land Changing Hands Config
    Made the option for chunks to change hands during a flag war toggle
    able.

--- src/com/palmergames/bukkit/towny/war/flagwar/listeners/TownyWarCustomListener.java
+++ src/com/palmergames/bukkit/towny/war/flagwar/listeners/TownyWarCustomListener.java
@@ -166,20 +166,22 @@ public class TownyWarCustomListener implements Listener {
 					x.printStackTrace();
 				}
 			}
-
-			// Defender loses townblock
-			//TownyUniverse.getDataSource().removeTownBlock(townBlock);
-
-			// Attacker Claim Automatically
-			//try {
-				//List<WorldCoord> selection = new ArrayList<WorldCoord>();
-				//selection.add(worldCoord);
-				//TownCommand.checkIfSelectionIsValid(attackingTown, selection, false, 0, false);
-				//new TownClaim(plugin, null, attackingTown, selection, false, true, false).start();
-			//} catch (TownyException te) {
-				// Couldn't claim it.
-			//}
-
+                        if (TownyWarConfig.cancapturechunks() == true) {
+                            // Defender loses townblock
+                            TownyUniverse.getDataSource().removeTownBlock(townBlock);
+                            // Attacker Claim Automatically
+                            try {
+                                    List<WorldCoord> selection = new ArrayList<WorldCoord>();
+                                    selection.add(worldCoord);
+                                    TownCommand.checkIfSelectionIsValid(attackingTown, selection, false, 0, false);
+                                    new TownClaim(plugin, null, attackingTown, selection, false, true, false).start();
+                            } catch (TownyException te) {
+                                    // Couldn't claim it.
+                            }
+			}
+                        else if (TownyWarConfig.cancapturechunks() == false) {
+                            //do nothing
+                        }
 			// Cleanup
 			plugin.updateCache(worldCoord);
 
